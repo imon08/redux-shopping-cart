aa59512511dc22313d9c7e5b18281cbae7f46d97
Priyosmita <priyosh08@gmail.com>
2023-01-29 18:05:10 +0530

    set definite data

--- src/component/Cart.js
+++ src/component/Cart.js
@@ -2,26 +2,31 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { getProducts, getTotalPrice } from "../slices/cartSlice";
 import CartItem from "./CartItem";
+import { AnimatedList } from "react-animated-list";
 
 const Cart = () => {
   const products = useSelector(getProducts);
-  const totalPrice = useSelector(getTotalPrice)
+  const totalPrice = useSelector(getTotalPrice);
   return (
     <div>
-      <h1 className="flex justify-center font-bold text-3xl m-3">SHOPPING CART</h1>
+      <h1 className="flex justify-center font-bold text-3xl m-3">
+        SHOPPING CART
+      </h1>
       <div className="flex flex-col gap-3 m-2 justify-center">
-        {products.map((item) => {
-          return (
-            <CartItem
-              key={item.product.id}
-              id={item.product.id}
-              name={item.product.name}
-              image={item.product.image}
-              price={item.product.price}
-              quantity={item.quantity}
-            />
-          );
-        })}
+        <AnimatedList animation={"grow"}>
+          {products.map((item) => {
+            return (
+              <CartItem
+                key={item.product.id}
+                id={item.product.id}
+                name={item.product.name}
+                image={item.product.image}
+                price={item.product.price}
+                quantity={item.quantity}
+              />
+            );
+          })}
+        </AnimatedList>
       </div>
       <div className="flex justify-end w-full text-2xl p-4 gap-4">
         <p>Total:</p>
--- src/component/Home.js
+++ src/component/Home.js
@@ -1,23 +1,66 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+// import { useEffect, useState } from "react";
+// import axios from "axios";
 import ProductCard from "./ProductCard";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { getProducts, getTotalQuantity } from "../slices/cartSlice";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
+const data = [
+  {
+    id: 1,
+    productName: "trimmer",
+    price: 1200,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo_W4j_6CwC91hFbU3sXFEA_XckJWB4RVFSw&usqp=CAU",
+  },
+  {
+    id: 2,
+    productName: "veet",
+    price: 250,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXUziWdquk8HkcpY4YZvUBjMB42sBR9yTXWQ&usqp=CAU",
+  },
+  {
+    id: 3,
+    productName: "handcream",
+    price: 450,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4-x7MsxYoJJVLc9xeilhLDqBP1XKNuWpIMA&usqp=CAU",
+  },
+  {
+    id: 4,
+    productName: "Mug",
+    price: 399,
+    image: "https://m.media-amazon.com/images/I/71NwoHMYaNL._SL1500_.jpg",
+  },
+  {
+    id: 5,
+    productName: "Runing Shoes",
+    price: 700,
+    image:
+      "https://m.media-amazon.com/images/I/71n9xHYpaHL._AC_UL480_FMwebp_QL65_.jpg",
+  },
+  {
+    id: 6,
+    productName: "Hoodie",
+    price: 919,
+    image: "https://m.media-amazon.com/images/I/71cEE8o1c2L._UX466_.jpg",
+  },
+];
+
 function Home() {
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
   const products = useSelector(getProducts);
   const totalQuantity = useSelector(getTotalQuantity)
 
   console.log(products);
 
-  useEffect(() => {
-    axios.get("http://localhost:4000/data").then((res) => {
-      setData(res.data);
-    });
-  }, []);
+  // useEffect(() => {
+  //   axios.get("http://localhost:4000/data").then((res) => {
+  //     setData(res.data);
+  //   });
+  // }, []);
 
   return (
     <div className="Home m-3 relative">
